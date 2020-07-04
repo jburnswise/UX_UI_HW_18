af0ed009f1d8224f02c0f5e091199285fd8ba91b
Jwise5 <31665258+Jwise5@users.noreply.github.com>
2020-07-03 23:23:36 -0700

    commitment

--- script/mobnavscript4.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('.menu');
-const btn = menu.querySelector('.nav-tgl');
-btn.addEventListener('click', evt => {
-  if (menu.className.indexOf('active') === -1) {
-		menu.classList.add('active');
-  } else {
-		menu.classList.remove('active');
-  }
-})
